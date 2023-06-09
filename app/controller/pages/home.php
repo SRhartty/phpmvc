544b25d2ec048bf707aa4b3cbf7ff13cb2951322
SR hartty <randonsspace@gmail.com>
2023-06-09 16:42:43 -0300

    git att

--- app/controller/pages/home.php
+++ app/controller/pages/home.php
@@ -3,16 +3,23 @@
 namespace App\Controller\Pages;
 
 use App\Utils\View;
+use App\Model\Entity\Organization;
 
 
 class Home extends Page{
     public static function getHome()
     {
+
+        $organization = new Organization();
+
         /**
          * metodo responsavel por retornar o honteudo da page home
          */
         $content =  View::render('pages/home',[
-            'nome' => 'Renderizado na Home',
+            'nome' => $organization->name,
+            'description' => $organization->description,
+            'site' => $organization->site
+
         ]);
         return parent::getPage('Home', $content);
     }
